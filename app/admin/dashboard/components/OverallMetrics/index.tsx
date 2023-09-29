68f2833e1236699ffed0382836993c6dd8e0b883
angelaldrete <angel.aldrete96@gmail.com>
2023-09-29 13:37:42 -0700

    Added use client to metrics

--- app/admin/dashboard/components/OverallMetrics/index.tsx
+++ app/admin/dashboard/components/OverallMetrics/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import useOverallMetrics from "../../hooks/useOverallMetrics";
 import MetricItem from "./MetricItem";
 
--- app/admin/dashboard/hooks/useOverallMetrics.tsx
+++ app/admin/dashboard/hooks/useOverallMetrics.tsx
@@ -1,3 +1,4 @@
+"use client";
 const useOverallMetrics = () => {
   const listItems: MetricCard[] = [
     {
